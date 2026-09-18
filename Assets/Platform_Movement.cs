5a82024d91e261b420217dadfee8de899711487f
michaeljmensah12-maker <michaelj.mensah12@gmail.com>
2026-09-18 12:52:26 -0400

    Created Rotating Platform

--- Assets/Platform_Movement.cs
+++ Assets/Platform_Movement.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Platform_Movement : MonoBehaviour
 {
-    public float moveSpeed = 10f; // speed of the platform
+    [SerializeField] private float moveSpeed = 10f; // speed of the platform
     private float timer = 0;
 
-    public float maxTimeMovingLeft = 15; // How long the platform moves left before switching direction
-    public float maxTimeMovingRight = 60; //Twice as long as left, so platform will return to 0 and move right for the same amount of time as left
+    [SerializeField] public float maxTimeMovingLeft = 15; // How long the platform moves left before switching direction
+    [SerializeField] public float maxTimeMovingRight = 60; //Twice as long as left, so platform will return to 0 and move right for the same amount of time as left
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
